0f85bcfe56823e751f1c89eb2fb2d8064033389a
yevhen-cherniuk <chernuy1@gmail.com>
2021-01-21 03:46:21 +0300

    Update hw-3-25.js

--- js/hw-3-25.js
+++ js/hw-3-25.js
@@ -1,7 +1,6 @@
 // Мы получили прогноз погоды на два дня, с минимальными и максимальными температурами, а также необязательными иконками.
 // Замени объявления всех переменных одной операцией деструктуризации свойств объекта forecast.
 // Задай значение по умолчанию для иконок, переменных todayIcon и tomorrowIcon - строку 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'.
-
 const forecast = {
   today: {
     low: 28,
@@ -15,6 +14,6 @@ const forecast = {
 };
 // Пиши код ниже этой строки
 
-const {today:{highToday, lowToday, todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg'},
-     tomorrow:{highTomorrow, lowTomorrow, icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' } } = forecast;
+const {today:{high:highToday, low:lowToday, icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'},
+     tomorrow:{high:highTomorrow, low:lowTomorrow, icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'}} = forecast;
 console.log(forecast);
